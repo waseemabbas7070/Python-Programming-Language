80b87d4b5e52a8458a02a82c9d4b6ee0ede3ac83
abc <abc@gmail.com>
2026-02-05 14:54:44 +0500

    Updated

---
+++ strings.py
@@ -0,0 +1,31 @@
+text = "My name is Waseem Abbas" 
+
+print(text.upper())          # Convert to uppercase
+
+print(text.lower())          # Convert to lowercase
+
+print(text.capitalize())     # Capitalize first letter
+
+print(text.title())          # Title case# Capitalize first letter of each word
+
+print(text.replace("Waseem", "Ahmed"))  # Replace substring
+
+print(text.split())         # Split into list of words 
+ 
+print(text.find("Abbas"))   # Find substring index
+
+print(text.startswith("My")) # Check if starts with substring
+
+print(text.endswith("Ali")) # Check if ends with substring
+
+print(" ".join(["Hello", "World"])) # Join list into string
+
+print(text.strip())        # Remove leading/trailing whitespace
+
+print(text.count("a"))    # Count occurrences of substring
+
+print(text.index("Waseem")) # Get index of substring
+
+print(text.isalpha())    # Check if all characters are alphabetic
+
+print(text.isdigit())    # Check if all characters are digits
---
+++ variables.py
@@ -0,0 +1,5 @@
+name = "Waseem"
+age = 23
+height = 1.57
+
+print(f"My namse is {name} , age is {age} and height is {height}")
