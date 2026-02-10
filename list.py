2f06b9001acfea34f1b35d3f63d5df3050c1c597
abc <abc@gmail.com>
2026-02-10 17:25:45 +0500

    Updated list

---
+++ list.py
@@ -0,0 +1,56 @@
+my_list = [2,1,3,4]
+print(my_list)
+
+print(my_list[0])
+
+# Negative Indexing
+
+print(my_list[-1])
+
+# Mutable
+
+my_list[2] = 11
+
+print(my_list)
+
+# Can store different data types
+
+max = [27,"Waseem",5.5,True]
+
+print(max)
+
+# Methods
+
+# Append()
+my_list.append(10)
+print(my_list)
+
+# Insert()
+
+my_list.insert(1,9) 
+
+print(my_list)
+
+# Remove()
+
+my_list.remove(9)
+
+print(my_list)
+
+# pop()
+
+my_list.pop(3)
+
+print(my_list)
+
+# Sort()
+
+my_list.sort()
+
+print(my_list)
+
+# Reverse list
+
+my_list.reverse()
+
+print(my_list)
